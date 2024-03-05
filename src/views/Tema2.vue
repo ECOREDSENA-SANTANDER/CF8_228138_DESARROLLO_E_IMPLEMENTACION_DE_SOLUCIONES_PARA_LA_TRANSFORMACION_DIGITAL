23fde49da7a472660a6100167df3a47eef406d81
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2024-03-05 16:23:56 -0500

    avance4

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -50,10 +50,30 @@
                 .col-lg-10.col-22.p-4
                   h4 Tarjeta de memoria SD: 
                   p La tarjeta SD usada para Raspberry Pi debe ser al menos clase 4 con mínimo 8 Gb de capacidad de almacenamiento, la tarjeta SD necesita ser instalada sobre un sistema operativo desde otro computador. El proceso de instalación puede  ser realizada sobre Windows, Mac, Linux, NOOBS y Raspbian.
-
         
-        
-
+      .container.color-contenedorpink.p-3
+        .row.justify-content-center.align-items-center(data-aos="fade-left")
+          .col-lg-1.col-22
+            img.p-1(src="@/assets/curso/tema2/fresa.png") 
+          .col-lg-10.col-22.p-3
+            h4 Ventajas de Raspberry Pi
+            ol.lista-ol
+              li 
+                span.text-bold • Uso sencillo
+              li 
+                span.text-bold • Tamaño muy pequeño
+              li 
+                span.text-bold • Precio bajo
+              li 
+                span.text-bold • Tienen pines de entrada/salida, por lo que también se pueden conectar a otros componentes electrónicos, como sensores, LEDs, etc.
+          
+      .row(data-aos="fade-left")
+        .col-lg-5.col-22.p-3
+          img(src="@/assets/curso/tema2/img03.png")
+        .col-lg-7.col-22.p-3
+          h5.p-2 Inventado en el Ivrea Interaction Design Institute, diseñado para la creación rápida de prototipos dirigido a aficionados, muy pronto su uso creció exponencialmente y se adapta a las últimas tendencias en el mercado, desde una placa de 8 bits hasta productos IoT, dispositivos portátiles y entornos embebidos.
+          p.p-2 El software Arduino es fácil de usar y se puede operar en plataformas Mac, Linux y Windows.<br><br>A continuación identifiquemos cada uno de sus componentes:
+  
   </template>
 
 <script>
